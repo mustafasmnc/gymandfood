18be1c25d44874b331f4fe67b214f2e9fd92f887
Mustafa <mustafasmnc@gmail.com>
2020-11-29 19:08:28 +0300

    FoodListNavigation

--- lib/model/food_category.dart
+++ lib/model/food_category.dart
@@ -26,16 +26,4 @@ final foodCategories = [
       foodCategoryDesc: "That is Drink Desc",
       foodCategoryImage:
           "https://images.squarespace-cdn.com/content/v1/5b4ee1897c93279fa2ecc59e/1568270381325-ZZNWR318V2BECL1EGX9P/ke17ZwdGBToddI8pDm48kOqo7Sx2tkTX1ZpqS4HCQ71Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyqkF6hrF03zgg83QPiQVuTKT-TJ630PzA1k9LbrN44qovtVJc1bPCxzTYeZ2TL2T4/istcoffe-k02G--621x414%40LiveMint.jpg?format=500w"),
-  FoodCategory(
-      foodCategoryId: "1",
-      foodCategoryName: "Food 2",
-      foodCategoryDesc: "That is Food 2 Desc",
-      foodCategoryImage:
-          "https://selfgrowth.info/photos/free-food-images-HD/best-food-wallpapers-without-words8426.jpg"),
-  FoodCategory(
-      foodCategoryId: "2",
-      foodCategoryName: "Drink 2",
-      foodCategoryDesc: "That is Drink 2 Desc",
-      foodCategoryImage:
-          "https://images.squarespace-cdn.com/content/v1/5b4ee1897c93279fa2ecc59e/1568270381325-ZZNWR318V2BECL1EGX9P/ke17ZwdGBToddI8pDm48kOqo7Sx2tkTX1ZpqS4HCQ71Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyqkF6hrF03zgg83QPiQVuTKT-TJ630PzA1k9LbrN44qovtVJc1bPCxzTYeZ2TL2T4/istcoffe-k02G--621x414%40LiveMint.jpg?format=500w"),
-];
+  ];
--- lib/ui/pages/food_list.dart
+++ lib/ui/pages/food_list.dart
@@ -1,5 +1,8 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:gymandfood/model/food.dart';
+
+import 'food_detail_screen.dart';
 
 class FoodList extends StatefulWidget {
   final String foodCatId;
@@ -72,57 +75,73 @@ class _FoodListState extends State<FoodList> {
                     //     ),
                     //   ),
                     // );
-                    return Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      padding: EdgeInsets.all(10),
-                      width: MediaQuery.of(context).size.width,
-                      height: 120,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
-                              color: Colors.black12,
-                            )
-                          ]),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(right: 10),
-                            width: 90,
-                            height: 90,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image:
-                                        NetworkImage(_filteredFoods[i].foodPic),
-                                    fit: BoxFit.cover),
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "${_filteredFoods[i].foodName}",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
+                    return OpenContainer(
+                        closedShape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        transitionDuration: const Duration(milliseconds: 1500),
+                        openBuilder: (context, _) {
+                          return FoodDetailScreen(food: _filteredFoods[i]);
+                        },
+                        closedBuilder: (context, openContainer) {
+                          return GestureDetector(
+                            onTap: openContainer,
+                            child: Container(
+                              margin: EdgeInsets.symmetric(vertical: 10),
+                              padding: EdgeInsets.all(10),
+                              width: MediaQuery.of(context).size.width,
+                              height: 120,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      blurRadius: 5,
+                                      offset: Offset(0, 3),
+                                      color: Colors.black12,
+                                    )
+                                  ]),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(right: 10),
+                                    width: 90,
+                                    height: 90,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                _filteredFoods[i].foodPic),
+                                            fit: BoxFit.cover),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${_filteredFoods[i].foodName}",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Container(
+                                        //width: MediaQuery.of(context).size.width / 1.65,
+                                        child: Text(
+                                          "Calorie: ${_filteredFoods[i].foodCal}",
+                                          maxLines: 2,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
                               ),
-                              SizedBox(height: 10),
-                              Container(
-                                //width: MediaQuery.of(context).size.width / 1.65,
-                                child: Text(
-                                  "Calorie: ${_filteredFoods[i].foodCal}",
-                                  maxLines: 2,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    );
+                            ),
+                          );
+                        });
                   }),
             )
           ],
